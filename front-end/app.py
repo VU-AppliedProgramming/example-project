b9e53799cfa76fc928387fadf20f29b7ced3b650
Mauricio Verano Merino <m.verano239@gmail.com>
2024-05-03 15:12:47 +0200

    cleaned up some code with the endpoints and made a simple example of using a form

--- front-end/app.py
+++ front-end/app.py
@@ -2,17 +2,32 @@ from flask import Flask, send_from_directory, jsonify, render_template, request,
 import requests
 from flask_cors import CORS
 from urllib.parse import urlencode
+import os
 
 app = Flask(__name__)
 CORS(app)
 
+BACKEND_ENDPOINT = os.environ.get('ENDPOINT')
+
 @app.route('/')
 def index():
+    print(BACKEND_ENDPOINT)
     return render_template('index.html')
 
 @app.route('/example')
 def example():
     return render_template('example.html')
+
+@app.route('/read-form', methods=['POST'])
+def read_form():
+    data = request.form
+
+    response = requests.get(f'{BACKEND_ENDPOINT}/api/meals2', params={'query': data['wordUser']})
+    meals = response.json()['results']
+
+    return render_template('results.html', results=meals)
+    
+
 
 @app.route('/api/meals')
 def search():
@@ -20,7 +35,7 @@ def search():
     min_calories = request.args.get('minCalories')
     max_calories = request.args.get('maxCalories')
     
-    backend_url = 'http://localhost:5000/api/meals'
+    backend_url = f'{BACKEND_ENDPOINT}/api/meals'
     response = requests.get(backend_url, params={'query': query, 'minCalories': min_calories, 'maxCalories': max_calories})
     print(response.json())
     # CHANGE HERE BY MARTON
@@ -29,19 +44,19 @@ def search():
 
 @app.route('/api/recipe/<meal_id>')
 def recipe(meal_id):
-    backend_url = f'http://localhost:5000/api/recipe/{meal_id}'
+    backend_url = f'{BACKEND_ENDPOINT}/api/recipe/{meal_id}'
     response = requests.get(backend_url)
     return jsonify(response.json())
 
 @app.route('/api/random')
 def get_random_recipe():
-    backend_url = f'http://localhost:5000/api/random'
+    backend_url = f'{BACKEND_ENDPOINT}/api/random'
     response = requests.get(backend_url)
     return jsonify(response.json())
 
 @app.route('/api/price_breakdown_widget/<int:meal_id>')
 def get_price_breakdown_widget(meal_id):
-    backend_url = f'http://localhost:5000/api/price_breakdown_widget/{meal_id}'
+    backend_url = f'{BACKEND_ENDPOINT}/api/price_breakdown_widget/{meal_id}'
     response = requests.get(backend_url)
     print(response.text) 
     return response.text
@@ -49,7 +64,7 @@ def get_price_breakdown_widget(meal_id):
 @app.route('/clean_html', methods=['POST'])
 def clean_html():
     html_string = request.data.decode("utf-8")
-    backend_url = 'http://localhost:5000/clean_html'
+    backend_url = f'{BACKEND_ENDPOINT}/clean_html'
     response = requests.post(backend_url, data=html_string)
     return jsonify(response.json())
 
